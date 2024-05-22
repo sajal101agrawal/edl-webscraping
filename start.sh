831bc1f8297641ad97f1214841baf300a33d3049
Nikunj117 <nikunjpansuriya117@gmail.com>
2024-05-22 19:17:44 +0530

    changes sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source /home/DickiData/edl-webscraping/env/bin/activate
+
+# Navigate to the project directory
+cd /home/DickiData/edl-webscraping/websoket_fol
+
+# Start the Gunicorn server with Uvicorn worker
+python api.py
