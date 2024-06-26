0b211257bed8e8d96600f39d70c10a977da21753
bhavin3206 <bhavinpatel99987@gmail.com>
2024-06-26 12:12:22 +0530

    update port

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
-import json
 
 app = Flask(__name__)
 
@@ -47,4 +46,4 @@ def get_data():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
